3e634f3c8db088d0a6b9e44384d365f340fec2b9
aliii390 <alli.said.errahmani@gmail.com>
2024-12-05 15:05:11 +0100

    ADD: setup + fin exo1

---
+++ exo1/index.php
@@ -0,0 +1,20 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    
+}
+
+if (
+    !isset(
+        $_POST['firstName'],
+        $_POST['lastName'],
+        
+       
+    )
+) {
+   
+}
+
+
+echo $_GET['firstname'];
+echo $_GET['lastname'];
